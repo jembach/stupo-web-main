471694d770eab7366c69ecdaeeef7374fde3b836
Jonas Embach <j.embach@bit-production.com>
2021-01-05 21:49:33 +0100

    chore: remove changelog file commit and package.json update

--- .releaserc.js
+++ .releaserc.js
@@ -1,18 +1,5 @@
 module.exports = {
-  branches: ['main', { name: 'staging', prerelease: true }],
-  verifyConditions: ['@semantic-release/changelog', '@semantic-release/git'],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    {
-      path: '@semantic-release/exec',
-      cmd: 'yarn run build',
-    },
-    {
-      path: '@semantic-release/git',
-      assets: ['CHANGELOG.md', 'package.json', 'yarn.lock'],
-    },
-  ],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
