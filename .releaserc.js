715ef4f1f688cdbc027cb4f523f277e8960f501b
Jonas Embach <j.embach@bit-production.com>
2021-01-05 21:21:02 +0100

    chore: remove sentry

--- .releaserc.js
+++ .releaserc.js
@@ -17,16 +17,5 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    [
-      '@eclass/semantic-release-sentry-releases',
-      {
-        sourcemaps: '.next',
-        urlPrefix: '~/_next',
-        environment: process.env.SENTRY_ENV,
-        repositoryUrl: process.env.REPOSITORY,
-        tagsUrl: `https://github.com/${process.env.REPOSITORY}/releases/tag`,
-        releasePrefix: process.env.SENTRY_PROJECT,
-      },
-    ],
   ],
 };
