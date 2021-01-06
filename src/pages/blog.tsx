ff0d9949e11d0c98188d67754f3192f6caacea3e
Jonas Embach <j.embach@bit-production.com>
2021-01-06 01:49:21 +0100

    feat: add blog page starter

---
+++ src/pages/blog.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from '../components/Header';
+import HeaderTitle from '../components/HeaderTitle';
+
+import { Page } from '../types';
+
+const Blog: Page<any> = () => (
+  <div>
+    <Header className="bg-stupo-blog-200" img="/images/headers/blog.svg" alt="">
+      <HeaderTitle>
+        <span>
+          Cookies, Tracker, Verschlüsselung -
+          <br /> Erfahre alles im
+        </span>
+        <span className="text-stupo-blog-complement-600"> #Blog</span>
+      </HeaderTitle>
+    </Header>
+  </div>
+);
+
+Blog.title = 'Blog | Stupo';
+Blog.keywords = 'Blog, IT-Sicherheit, Datenschutz, News, Cyberangriffe, Wissenswertes';
+Blog.description =
+  'Im Bereich Blog findest du aktuelle News zu IT-Sicherheit, Wissenswertes, wie du dich schützen kannst und vieles mehr.';
+
+export default Blog;
