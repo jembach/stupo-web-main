32f85541fa3f540a6505b8c658c9d845387868fb
Jonas Embach <j.embach@bit-production.com>
2021-01-06 01:27:09 +0100

    chore: set page author, DC.title and generator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,14 +19,14 @@ function MyApp({
       <Head>
         <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
 
-        <meta name="DC.title" content="PASTE_TITLE_HERE" />
+        <meta name="DC.title" content="Stupo - Die Medienbildungsplatform" />
 
         <title>{Component.title}</title>
         <meta name="description" content={Component.description} />
         <meta name="keywords" content={Component.keywords} />
 
-        <meta name="author" content="REPLACE_THIS" />
-        <meta name="generator" content="PASTE_GENERATOR_NAME_HERE - PASTE_DOMAIN_HERE" />
+        <meta name="author" content="Jonas Embach <j.embach@bit-production.com>" />
+        <meta name="generator" content="Jonas Embach - j.embach@bit-production.com" />
         <meta name="robots" content="index,follow,noodp,noydir" />
         <meta name="revisit-after" content="7 days" />
         <link type="image/x-icon" href="favicon.ico" rel="shortcut icon" />
