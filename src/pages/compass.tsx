f7a597c66372080f04d791b645501c207148169d
Jonas Embach <j.embach@bit-production.com>
2021-01-06 01:49:31 +0100

    feat: add compass page starter

---
+++ src/pages/compass.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from '../components/Header';
+import HeaderTitle from '../components/HeaderTitle';
+
+import { Page } from '../types';
+
+const Compass: Page<any> = () => (
+  <div>
+    <Header className="bg-stupo-compass-200" img="/images/headers/compass.svg" alt="">
+      <HeaderTitle>
+        <span>Den richtigen weg gehen mit dem digitalen</span>
+        <span className="text-stupo-compass-complement-600"> #Kompass</span>
+      </HeaderTitle>
+    </Header>
+  </div>
+);
+
+Compass.title = 'IT-Kompass | Stupo';
+Compass.keywords = 'IT-Kompass, Schüler, Medienbildung';
+Compass.description =
+  'Der IT-Kompass ist ein an der Otto-von-Guericke Universität entwickelter Leitfaden wie du sicher und ohne auszuspioniert zu werden das Internet benutzen kannst.';
+
+export default Compass;
