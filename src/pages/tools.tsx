84dce0f30d7ecbc6189a954cad1d1b5ac9865e7b
Jonas Embach <j.embach@bit-production.com>
2021-01-06 01:50:36 +0100

    feat: add tools page starter

---
+++ src/pages/tools.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../components/Header';
+import HeaderTitle from '../components/HeaderTitle';
+
+import { Page } from '../types';
+
+const Tools: Page<any> = () => (
+  <div>
+    <Header className="bg-stupo-tools-200" img="/images/headers/tools.svg" alt="">
+      <HeaderTitle>
+        <span>
+          Was passiert wenn ich?
+          <br />
+          Prüfe deine bedenken sicher
+        </span>
+        <span className="text-stupo-tools-complement-600"> #Tools</span>
+      </HeaderTitle>
+    </Header>
+  </div>
+);
+
+Tools.title = 'Tools | Stupo';
+Tools.keywords = 'Tools, IT-Sicherheit';
+Tools.description =
+  'Im Bereich Tools findest du kleine interaktive Anwendungen, mit denen du IT-Sicherheitsaspekte testen kannst.';
+
+export default Tools;
