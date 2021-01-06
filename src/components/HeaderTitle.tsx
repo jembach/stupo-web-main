c0ce5b03d6b909d419361b81c5735ad12eca722b
Jonas Embach <j.embach@bit-production.com>
2021-01-06 13:49:18 +0100

    chore(HeaderTitle): transform inline css to css module

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
+import styles from './headerTitle.module.css';
+
 function HeaderTitle({
   children,
 }: {
   children: React.ReactElement | React.ReactElement[];
 }): JSX.Element {
-  return (
-    <span className="text-4xl md:text-5xl leading-normal font-bold uppercase tracking-wide">
-      {children}
-    </span>
-  );
+  return <span className={styles['header-title']}>{children}</span>;
 }
 
 export default HeaderTitle;
