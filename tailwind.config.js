fde118ccb81077989a59d1687d8e7ad1e0d96751
Jonas Embach <j.embach@bit-production.com>
2021-01-23 21:19:04 +0100

    feat: add compass page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 const colors = require('tailwindcss/colors');
+const pluginTypography = require('@tailwindcss/typography');
 
 module.exports = {
   purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
@@ -8,25 +9,27 @@ module.exports = {
     container: {
       padding: '1.5rem',
     },
-    colors: {
-      white: '#fff',
-      textColor: '#080539',
-      'stupo-main': colors.lightBlue,
-      'stupo-main-complement': colors.orange,
-      'stupo-compass': colors.purple,
-      'stupo-compass-complement': colors.lightBlue,
-      'stupo-quiz': colors.yellow,
-      'stupo-quiz-complement': colors.lightBlue,
-      'stupo-blog': colors.orange,
-      'stupo-blog-complement': colors.rose,
-      'stupo-tools': colors.red,
-      'stupo-tools-complement': colors.indigo,
-      'stupo-pupil': colors.lime,
-      'stupo-pupil-complement': colors.purple,
-      'stupo-teacher': colors.rose,
-      'stupo-teacher-complement': colors.green,
+    extend: {
+      colors: {
+        white: '#fff',
+        textColor: '#080539',
+        'stupo-main': colors.lightBlue,
+        'stupo-main-complement': colors.orange,
+        'stupo-compass': colors.purple,
+        'stupo-compass-complement': colors.lightBlue,
+        'stupo-quiz': colors.yellow,
+        'stupo-quiz-complement': colors.lightBlue,
+        'stupo-blog': colors.orange,
+        'stupo-blog-complement': colors.rose,
+        'stupo-tools': colors.red,
+        'stupo-tools-complement': colors.indigo,
+        'stupo-pupil': colors.lime,
+        'stupo-pupil-complement': colors.purple,
+        'stupo-teacher': colors.rose,
+        'stupo-teacher-complement': colors.green,
+      },
     },
   },
   variants: { extend: { padding: ['first', 'last'] } },
-  plugins: [],
+  plugins: [pluginTypography],
 };
